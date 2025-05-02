4ddfe8d04540d159fdd38e8f8c391eedd1774736
praharshamaroju <marojupraharsha@gmail.com>
2025-05-02 11:18:35 +0530

    ex1-sol-console-code

---
+++ ex1-console-code.js
@@ -0,0 +1,33 @@
+alert('hello');
+undefined
+alert('good job');
+undefined
+2 + 2
+4
+10 - 3
+7
+document.body.innerHTML = 'HELLO' ;   
+'HELLO'
+alert('Good morning');   
+undefined
+allow pasting
+alert('Praharsha Maroju');
+undefined
+10  + 5
+15
+2+2-5
+-1
+document.innerHTML='Good Morning!'
+'Good Morning!'
+document.body.innerHTML = 'Good morning!' ;
+'Good morning!'
+document.body.innerHTML = 'Praharsha Maroju' ;
+'Praharsha Maroju'
+10 + 8 + 20
+38
+100-20-50+200
+230
+document.innerHTML =' ';
+' '
+document.body.innerHTML = ' ' ;
+' '
