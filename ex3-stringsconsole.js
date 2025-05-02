7e97fcc8ca6e56907e75577f8d4d0cc64398a983
praharshamaroju <marojupraharsha@gmail.com>
2025-05-02 15:44:51 +0530

    ex3 solutions

---
+++ ex3-stringsconsole.js
@@ -0,0 +1,80 @@
+'hello'
+'hello'
+"hello"
+'hello'
+"i'm learning js"
+"i'm learning js"
+'i\'m learning js'
+"i'm learning js"
+alert('some\ntext');
+undefined
+`hello`
+'hello'
+`items(${1+1}):${(2095+799)/100}`
+'items(2):28.94'
+`some
+text'
+
+`some
+VM2966:4 Uncaught SyntaxError: Unexpected identifier 'some'Understand this error
+`some
+text'
+'my name is : Praharsha maroju'
+
+VM3095:4 Uncaught SyntaxError: Unexpected end of inputUnderstand this error
+'my name is: praharsha'
+'my name is: praharsha'
+'praharsha'
+'praharsha'
+'my name is: '+'harshaa'
+'my name is: harshaa'
+'Total cost(' + (1+1) + '):$' + (5+3)
+'Total cost(2):$8'
+`total cost(${1+1}):${(500+300)/100}`
+'total cost(2):8'
+alert(`total cost(${1+1}):${(500+300)/100}`)
+alert(`total cost(${1+1}) : ${(599+295)/100}`)
+undefined
+alert(`total cost(${1+1}) : ${(599+295)/100`\n 'Thank you,come again!')
+alert(`total cost(${1+1}) : ${(599+295)/100`\n 'Thank you,come again!')
+VM3929:1 Uncaught SyntaxError: Missing } in template expressionUnderstand this error
+alert(`total cost(${1+1}) : ${(599+295)/100}`\n 'Thank you,come again!')
+
+VM3933:1 Uncaught SyntaxError: missing ) after argument listUnderstand this error
+alert(`total cost(${1+1}) : ${(599+295)/100} Thank you,come again!')
+VM3943:1 Uncaught SyntaxError: Unexpected end of inputUnderstand this error
+alert(`total cost(${1+1}) : ${(599+295)/100} 
+    Thank you,come again!`);
+undefined
+alert(`total cost(${1+1}) : ${(599+295)/100} 
+Thank you,come again!`);
+alert(`total cost(${1+1}) : ${(599+295)/100} Thank you,come again!`);
+undefined
+undefined
+alert(`total cost(${1+1}) : ${(599+295)/100} Thank you,come again!`);
+undefined
+alert(`total cost(${1+1}) : ${(599+295)/100} Thank you,come again!`);
+undefined
+alert(`total cost(${1+1}) : ${(599+295)/100} 
+Thank you,come again!`);
+undefined
+`total cost(${2+2}): ${(2095*2+799*2 + 998)/100}`
+'total cost(4): 67.86'
+`total cost (${2+2}):${(2095*2+799*2+998)/100}`
+'total cost (4):67.86'
+`shipping & handling:$9.98`
+'shipping & handling:$9.98'
+`total before tax: ${(2095*2+799*2+998)/100}`
+'total before tax: 67.86'
+`estimated tax (10%):${Math.round((2095*2+799*2+998)/100 * 10/100)
+VM4765:1 Uncaught SyntaxError: Missing } in template expressionUnderstand this error
+`estimated tax (10%):${Math.round((2095*2+799*2+998)/100 * 10/100)}`
+'estimated tax (10%):7'
+`estimated tax (10%):$${Math.round((2095*2+799*2+998)/100 *0.1)/100}`
+'estimated tax (10%):$0.07'
+`estimated tax (10%):$${(Math.round(67.86*10/100)}`
+VM5003:1 Uncaught SyntaxError: Unexpected token '}'Understand this error
+`estimated tax (10%):$${(Math.round(67.86*10/100)}`
+VM5011:1 Uncaught SyntaxError: Unexpected token '}'Understand this error
+`estimated tax(10%) : ${(Math.round(67.86*0.1)}`
+VM5230:1 Uncaught SyntaxError: Unexpected token '}'Understand this error
